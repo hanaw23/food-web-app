7c59d4cc68cc7985c810e8b6e4ee4a8034cab26f
Hana Widyatari <77064807+hanaw23@users.noreply.github.com>
2023-10-18 02:25:16 +0700

    feat: fetch API

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  product: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="card">
+    <img :src="`assets/images/${product.gambar}`" class="card-img-top" alt="assets/images/default.png" />
+    <div class="card-body">
+      <h5 class="card-title">{{ product.nama }}</h5>
+      <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a> -->
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,63 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import CardProduct from "../components/CardProduct.vue";
+
+const products = ref([]);
+const pagination = ref({
+  page: 1,
+  limit: 3,
+});
+const disabledLoadMore = ref(false);
+
+// Fetch data from API
+onMounted(async () => {
+  getProductMethods();
+});
+const getProductMethods = async () => {
+  axios({
+    method: "get",
+    url: "http://localhost:3000/products?_page=1&_limit=3",
+    responseType: "stream",
+  })
+    .then((response) => (products.value = JSON.parse(response.data)))
+    .catch((error) => console.log(error));
+};
+
+const loadMore = async () => {
+  const urlWebProducts = `http://localhost:3000/products?_page=${(pagination.value.page += 1)}&_limit=${pagination.value.limit}`;
+  axios({
+    method: "get",
+    url: urlWebProducts,
+    responseType: "stream",
+  })
+    .then((response) => {
+      if (JSON.parse(response.data).length === 0) {
+        disabledLoadMore.value = true;
+      } else {
+        products.value.push(...JSON.parse(response.data));
+      }
+    })
+    .catch((error) => console.log(error));
+};
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <div class="row mt-4">
+      <div class="col">
+        <h2>Best <strong>Foods</strong></h2>
+      </div>
+      <div class="col">
+        <RouterLink to="/foods" class="btn btn-success float-right">View all</RouterLink>
+      </div>
+    </div>
+
+    <div class="row mb-3">
+      <div v-for="product in products" :key="product.id">
+        <CardProduct :product="product" />
+      </div>
+      <button type="button" class="btn btn-primary" :disabled="disabledLoadMore" @click="loadMore">Load More</button>
+    </div>
+  </div>
 </template>
